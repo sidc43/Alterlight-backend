61c35dd4b6d6e974ba0589afbb10c47e26b5a3bd
xdsid123 <imadethisjustfororiginbruh@gmail.com>
2023-05-01 21:54:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -155,4 +155,4 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, './404.html'));
 });
 
-app.listen(port, (e) => (e ? print(e) : print(`Running on port ${port}`)));
+app.listen(port, (e) => (e ? print(`Error ${e}`) : print(`Running on port ${port}`)));
